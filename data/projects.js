177e8d1eca59341e55f0c2e48893078bd84d61c3
ikekyzero <ikekyzero@users.noreply.github.com>
2025-01-18 00:34:49 +0000

    deploy: b39636d8bb93e60ed6243e77560d87224e4f2cd6

---
+++ data/projects.js
@@ -0,0 +1,90 @@
+export const projectsData = {
+    'finding-woman': {
+        icon: './assets/1.png',
+        markdownPath: './data/projects/Находчивая женщина.md'
+    },
+    'jipto': {
+        icon: './assets/2.png',
+        markdownPath: './data/projects/ЖИПТО.md'
+    },
+    'happy-parent': {
+        icon: './assets/7.jpg',
+        markdownPath: './data/projects/HappyParent.md'
+    },
+    'quest-platformer': {
+        icon: './assets/8.png',
+        markdownPath: './data/projects/2D квест-платформер.md'
+    },
+    'aifhoc': {
+        icon: './assets/20.jpg',
+        markdownPath: './data/projects/AIFHOC.md'
+    },
+    'salemart': {
+        icon: './assets/6.png',
+        markdownPath: './data/projects/Salemart.md'
+    },
+    'sports-manager': {
+        icon: './assets/14.jpg',
+        markdownPath: './data/projects/Менеджер спортивных мероприятий.md'
+    },
+    'gravity-falls': {
+        icon: './assets/11.png',
+        markdownPath: './data/projects/Поиск предметов в мире Гравити Фолз.md'
+    },
+    'mytona-game': {
+        icon: './assets/18.jpg',
+        markdownPath: './data/projects/Игра во время стажировки в MYTONA.md'
+    },
+    'damn-terrarium': {
+        icon: './assets/5.jpg',
+        markdownPath: './data/projects/Damn Terrarium.md'
+    },
+    'go-help': {
+        icon: './assets/9.jpg',
+        markdownPath: './data/projects/GO HELP.md'
+    },
+    'legend-of-office': {
+        icon: './assets/3.png',
+        markdownPath: './data/projects/Legend of Office.md'
+    },
+    'the-companions': {
+        icon: './assets/10.jpg',
+        markdownPath: './data/projects/theCompanions.md'
+    },
+    'it-specialist': {
+        icon: './assets/19.jpg',
+        markdownPath: './data/projects/Айтишник.md'
+    },
+    'first-shooter': {
+        icon: './assets/17.webp',
+        markdownPath: './data/projects/Мой первый шутер на Unity.md'
+    },
+    'steins-gate': {
+        icon: './assets/16.gif',
+        markdownPath: './data/projects/Поймать вещи в антураже Steins;Gate.md'
+    },
+    'china-adventure': {
+        icon: './assets/13.png',
+        markdownPath: './data/projects/Приключения в Китае.md'
+    },
+    'movie-site': {
+        icon: './assets/15.png',
+        markdownPath: './data/projects/Сайт кинолент.md'
+    },
+    'jojo-fighting': {
+        icon: './assets/12.webp',
+        markdownPath: './data/projects/Файтинг Джоджо.md'
+    }
+};
+
+// Данные для длинных статей
+export const longsData = {
+    'drive-my-car': {
+        id: 'drive-my-car',
+        title: 'Обзор: Сядь за руль моей машины',
+        description: 'Обзор японского фильма, получившего "Оскар"',
+        markdownPath: './data/longs/long_1.md',
+        date: '2024-03'
+    }
+    // Здесь можно добавлять другие статьи
+};
